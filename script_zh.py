724bff728734dceadfcdd78f78270482e4c4d15b
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-08-31 23:12:26 +0800

    Class of iCal

--- script_zh.py
+++ script_zh.py
@@ -252,7 +252,7 @@ def random_str(randomlength):
     chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789'
     length = len(chars) - 1
     random = Random()
-    for i in range(randomlength):
+    for _ in range(randomlength):
         str += chars[random.randint(0, length)]
     return str
 
