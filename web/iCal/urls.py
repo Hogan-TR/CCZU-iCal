8b51366c9a9b770af87040be817493f85062e27e
15262856393tr@gmail.com <15262856393tr@gmail.com>
2019-09-02 14:29:26 +0800

    almost done

--- web/iCal/urls.py
+++ web/iCal/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('subscribe/',views.subscribe, name='subscribe'),
+    path('download/<str:filename>/',views.download, name='download'),
 ]
