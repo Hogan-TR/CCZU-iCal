090b4ecd05f9bf9efcf670b0a77b1eec4a8c1496
Hogan-TR <15262856393tr@gmail.com>
2020-03-07 22:31:47 +0800

    change for producting env

--- web/config.py
+++ web/config.py
@@ -2,9 +2,9 @@ import os
 
 
 RUN_HOST = '0.0.0.0'
-RUN_PORT = '8888'
+RUN_PORT = '5000'
 
-BASE_IP = "localhost" # Change IP with real ip or domain name
+BASE_HOST = "ical.hogantr.me" # Change IP with real ip or domain name
 
 SECRET_KEY = os.urandom(24)
 
